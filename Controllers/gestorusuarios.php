b163b5fd8b8a3003210a62d1b98533d5303cad0b
Jose Carlos <50499480+jcavila44@users.noreply.github.com>
2021-10-26 10:03:55 -0500

    Update CRUD gestor usuarios 2021-10-26
    Update CRUD gestor usuarios 2021-10-26

--- Controllers/gestorusuarios.php
+++ Controllers/gestorusuarios.php
@@ -43,7 +43,7 @@ class GestorUsuarios extends Facade
 			$arrData = $this->obtenerUsuarios();
 			$arrRespuesta = array('status' => 'success', 'data' => $arrData);
 		} else {
-			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al metodo');
+			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al método');
 		}
 		echo json_encode($arrRespuesta, JSON_UNESCAPED_UNICODE);
 		die();
@@ -57,7 +57,7 @@ class GestorUsuarios extends Facade
 			$arrData = $this->getRoles();
 			$arrRespuesta = array('status' => 'success', 'data' => $arrData);
 		} else {
-			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al metodo');
+			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al método');
 		}
 		echo json_encode($arrRespuesta, JSON_UNESCAPED_UNICODE);
 		die();
@@ -80,7 +80,54 @@ class GestorUsuarios extends Facade
 				$arrRespuesta = array('status' => 'error', 'msg' => 'No se ha podido registrar');
 			}
 		} else {
-			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al metodo');
+			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al método');
+		}
+		echo json_encode($arrRespuesta, JSON_UNESCAPED_UNICODE);
+		die();
+	}
+
+	//Metodo para obtener todos los usuarios
+	public function deleteUserController()
+	{
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+			$idUsuario = limpiar_cadena($_POST['idUsuario']);
+
+			$peticion = $this->deleteUsuario($idUsuario);
+
+			if ($peticion > 0) {
+				$arrRespuesta = array('status' => 'success', 'msg' => 'Usuario agregado satisfactoriamente');
+			} else {
+				$arrRespuesta = array('status' => 'error', 'msg' => 'No se ha podido registrar');
+			}
+		} else {
+			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al método');
+		}
+		echo json_encode($arrRespuesta, JSON_UNESCAPED_UNICODE);
+		die();
+	}
+
+
+	//Metodo para obtener todos los usuarios
+	public function actualizarInfoUserController()
+	{
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+			$nombreUsuario = limpiar_cadena($_POST['nombreUsuario']);
+			$cedulaUsuario = limpiar_cadena($_POST['cedulaUsuario']);
+			$CorreoUsuario = limpiar_cadena($_POST['CorreoUsuario']);
+			$rolUsuario = limpiar_cadena($_POST['rolUsuario']);
+			$usu_id = limpiar_cadena($_POST['usu_id']);
+
+			$peticion = $this->updateUsuario($nombreUsuario, $cedulaUsuario, $CorreoUsuario, $rolUsuario, $usu_id);
+
+			if ($peticion > 0) {
+				$arrRespuesta = array('status' => 'success', 'msg' => 'Usuario actualizaco correctamente');
+			} else {
+				$arrRespuesta = array('status' => 'error', 'msg' => 'No se logró actualizar');
+			}
+		} else {
+			$arrRespuesta = array('status' => 'error', 'msg' => 'La peticion HTTP, no corresponde al método');
 		}
 		echo json_encode($arrRespuesta, JSON_UNESCAPED_UNICODE);
 		die();
